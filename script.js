c0c19f72ea15fe0d936ad697ea4ce282cd97576e
Megan Russell <megan.russell50@gmail.com>
2021-01-12 17:22:59 -0800

    trying to put files on main branch

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// // Assignment Code
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 //
@@ -14,9 +14,6 @@ function promptUser(){
 }
 promptUser()
 
-function promptUser(){
-var symbols = prompt("Would you like to include special characters?")
-}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -26,48 +23,5 @@ function writePassword() {
 
 }
 
-// // Add event listener to generate button
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// DOM Elements
-// const resultEl = document.getElementById('result');
-// const lengthEl = document.getElementById('length');
-// const uppercaseEl = document.getElementById('uppercase');
-// const lowercaseEl = document.getElementById('lowercase');
-// const numbersEl = document.getElementById('numbers');
-// const symbolsEl = document.getElementById('symbols');
-// const generateEl = document.getElementById('generate');
-
-
-
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-};
-
-
-// Generator functions  http://www.net-comber.com/charset.html
-
-function getRandomLower() { 
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-
-function getRandomUpper() { 
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-
-function getRandomNumber() { 
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-
-}
-
-function getRandomSymbol() { 
-  const symbols = '＆＠＃％＋－＊＝＜＞()[]{｝'
-  return symbols[Math.floor(Math.random() * symbols.length)]
-}
-
-console.log(getRandomSymbol())
